462f2f9c564f06efaf2215b49bf178a19000a22b
Sonnysboy <sonnysboy4@gmail.com>
2019-11-19 23:29:40 +0000

    i did stuff

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
-// import three as three
-let torusGeometry = new THREE.TorusGeometry(7, 1.6, 4, 3, 6.3);
-let material = new THREE.MeshBasicMaterial({ color: 0x0071C5 });
-let torus = new THREE.Mesh(torusGeometry, material);
-// scene.add(torus);
-
-// Update mesh rotation using quaternion.
-const sensorAbs = new AbsoluteOrientationSensor();
-sensorAbs.onreading = () => torus.quaternion.fromArray(sensorAbs.quaternion);
-sensorAbs.start();
-
-// Update mesh rotation using rotation matrix.
-const sensorRel = new RelativeOrientationSensor();
-let rotationMatrix = new Float32Array(16);
-sensor_rel.onreading = () => {
-    sensorRel.populateMatrix(rotationMatrix);
-    torus.matrix.fromArray(rotationMatrix);
+function initSensor() {
+    sensor = new AbsoluteOrientationSensor({frequency: 60});
+    sensor.onreading = () => model.quaternion.fromArray(sensor.quaternion);
+    sensor.onerror = event => {
+        if (event.error.name == 'NotReadableError') {
+            console.log("Sensor is not available.");
+        }else {
+          alert('can');
+        }
+    }
+    sensor.start();
 }
-sensorRel.start();
+initSensor();
